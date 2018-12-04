781b78770fbfb8e65f44a8a63942ab2f142a5c90
杨俊宁 <1003719811@qq.com>
2018-12-04 11:15:00 +0800

    hello world

--- routes/wechat.js
+++ routes/wechat.js
@@ -11,26 +11,7 @@ const config = {
 
 router.use('/', wechat(config).middleware(async (message, ctx) => {
   console.log('收到微信消息：', { message })
-  // 微信输入信息就是这个 message
-  switch (message.FormUserName) {
-    case '屌丝':
-      return '呵呵'
-    case 'text':
-      // 你也可以这样回复text类型的消息
-      return {
-        content: 'text object',
-        type: 'text',
-      }
-    default:
-      return [
-        {
-          title: '你来我家接我吧',
-          description: '这是女神与高富帅之间的对话',
-          picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-          url: 'http://nodeapi.cloudfoundry.com/',
-        },
-      ]
-  }
+  return 'Hello World!'
 }))
 
 module.exports = router
