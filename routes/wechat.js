21054243dbb9bb84f253c855d943188e53b42225
杨俊宁 <1003719811@qq.com>
2018-12-04 10:40:17 +0800

    wechat

--- routes/wechat.js
+++ routes/wechat.js
@@ -16,6 +16,26 @@ const config = {
   encodingAESKey,
 }
 
-router.get('/', async (ctx) => {
-
-})
+router.get('/', wechat(config).middleware(async (message, ctx) => {
+  console.log(message, ctx)
+  // 微信输入信息就是这个 message
+  switch (message.FormUserName) {
+    case '屌丝':
+      return '呵呵'
+    case 'text':
+      // 你也可以这样回复text类型的消息
+      return {
+        content: 'text object',
+        type: 'text',
+      }
+    default:
+      return [
+        {
+          title: '你来我家接我吧',
+          description: '这是女神与高富帅之间的对话',
+          picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+          url: 'http://nodeapi.cloudfoundry.com/',
+        },
+      ]
+  }
+}))
