d53119fcae83f675a3f6d9eda0e2715057c549f6
杨俊宁 <1003719811@qq.com>
2018-12-04 13:08:08 +0800

    实现对话

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router')
 const { wechat } = require('../utils/wx')
 
-const router = new Router({ prefix: '/wechat' })
+const router = new Router({ prefix: '/wx' })
 
-router.post('/', wechat(async (message, ctx) => {
-  console.log(`收到的消息：{
-    ${message}
-  }`)
+router.get('/', wechat()) // 用于服务器配置校验
+
+router.post('/', wechat(async (message) => {
+  console.log('收到的消息:', message)
   const { ToUserName, FromUserName, CreateTime, MsgType, Content, MsgId } = message
   return [
     {
