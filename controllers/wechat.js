9559ef9b21604ab8aa7d91a4b13e0d08b1f96631
杨俊宁 <1003719811@qq.com>
2018-12-21 19:39:18 +0800

    实现点击按钮发送消息

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -10,31 +10,43 @@ const wechatConfig = wechat(config).middleware()
 
 const wechatMessageService = wechat(config).middleware(async (message, ctx) => {
   console.log('收到的消息:', message)
-  switch (message.MsgType) {
-    case 'text':
-      if (message.Content === 'link') {
-        return '<a href="http://wpa.qq.com/msgrd?V=1&Uin=806719384">QQ咨询预约</a>'
+  const { MsgType, Event, EventKey } = message
+  if (MsgType === 'event') {
+    if (Event === 'subscribe') {
+      return '终于等到你，还好没放弃'
+    } else if (Event === 'CLICK') {
+      if (EventKey === 'song') {
+        return '今天并没有歌！'
       }
-      return '普通文本消息'
-    case 'image':
-      return {
-        type: 'image',
-        content: {
-          mediaId: message.MediaId,
-        },
-      }
-    case 'voice':
-      return {
-        type: 'voice',
-        content: {
-          mediaId: message.MediaId,
-        },
-      }
-    case 'location':
-      return `您的位置：${message.Label}`
-    default:
-      return '你发送了不支持的消息类型！'
+    }
   }
+  return '非事件消息'
+
+  // switch (message.MsgType) {
+  //   case 'text':
+  // if (message.Content === 'link') {
+  //   return '<a href="http://wpa.qq.com/msgrd?V=1&Uin=806719384">QQ咨询预约</a>'
+  // }
+  //     return '普通文本消息'
+  //   case 'image':
+  //     return {
+  //       type: 'image',
+  //       content: {
+  //         mediaId: message.MediaId,
+  //       },
+  //     }
+  //   case 'voice':
+  //     return {
+  //       type: 'voice',
+  //       content: {
+  //         mediaId: message.MediaId,
+  //       },
+  //     }
+  //   case 'location':
+  //     return `您的位置：${message.Label}`
+  //   default:
+  //     return '你发送了不支持的消息类型！'
+  // }
 })
 
 const stuffBindPage = async (ctx) => {
